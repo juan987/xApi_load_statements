fbe40802a9c47e23bd2c423559eae45deaa237e3
Juan Miguel Navarro <JE10822@educaterra.com>
2017-02-28 19:25:38 +0100

    actualice el doc de miscelaneous

--- xApiStatements/doc_miscelaneaus.js
+++ xApiStatements/doc_miscelaneaus.js
@@ -15,13 +15,28 @@ db.statements.aggregate([
 //prueba de aggragate con padre
 db.statements.aggregate([
     { $group: {
-        _id: {miObjeto:"$target", parent:"$contextActivities.parent.id"},
+        _id: {miObjeto:"$target.id", parent:"$context.contextActivities.parent.id"},
         num: { $sum: 1 }
     } },
     { $sort: { _id: 1 } },
     {
         $project:{
             _id:0, objeto:"$_id.miObjeto" , parent:"$_id.parent"
+        }
+    },
+])
+
+//prueba de agregate con parent primero
+db.statements.aggregate([
+    {$project:{"context.contextActivities.parent.id" :1, "target.id":1, _id :0}},
+    { $group: {
+        _id: {parent:"$context.contextActivities.parent.id", miObjeto:"$target.id" },
+        num: { $sum: 1 }
+    } },
+    { $sort: { _id: 1 } },
+    {
+        $project:{
+           parent:"$_id.parent",  _id:0, actividad:"$_id.miObjeto" 
         }
     },
 ])
